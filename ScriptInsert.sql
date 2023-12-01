1dd60053e7a468f9fd46c909362489ab0a92c4ec
PriyankSolanki <psolanki@condorcet93.fr>
2023-12-01 17:30:04 +0100

    modification du script sql ajout de insert

--- ScriptInsert.sql
+++ ScriptInsert.sql
@@ -14,10 +14,11 @@ INSERT INTO Degrees (name) VALUES ('INFOCOM');
 -- Insertion des utilisateurs
 INSERT INTO Users (username, password, last_name, first_name, email, isAdmin, id_Role) VALUES ('admin', 'password', 'Admin', 'Admin', 'admin@example.com', true, 1);
 INSERT INTO Users (username, password, last_name, first_name, email, isAdmin, id_Role) VALUES ('user', 'password', 'User', 'User', 'user@example.com', false, 2);
+INSERT INTO Users (username, password, last_name, first_name, email, isAdmin, id_Role) VALUES ('student', 'password', 'Admin', 'Admin', 'admin@example.com', true, 3);
+INSERT INTO Users (username, password, last_name, first_name, email, isAdmin, id_Role) VALUES ('teacher', 'password', 'User', 'User', 'user@example.com', false, 4);
 
 -- Insertion des enseignants
-INSERT INTO Teachers (initial, desktop, id_User) VALUES ('T1', 'Desk1', 1);
-INSERT INTO Teachers (initial, desktop, id_User) VALUES ('T2', 'Desk2', 2);
+INSERT INTO Teachers (initial, desktop, id_User) VALUES ('T1', 'Desk1', 4);
 
 -- Insertion des formations
 INSERT INTO Trainings (name, id_Degree) VALUES ('Formation1', 1);
@@ -48,20 +49,18 @@ INSERT INTO Classroom (name, capacity) VALUES ('Salle1', 30);
 INSERT INTO Classroom (name, capacity) VALUES ('Salle2', 40);
 
 -- Insertion des cours
-INSERT INTO Courses (startTime, endTime, dateCourse, control, id_Resource, id_Tp, id_Td, id_Promotion, id_Teacher, id_classroom) VALUES ('08:00:00', '10:00:00', '2023-11-15', true, 1, 1, 1, 1, 1, 1);
-INSERT INTO Courses (startTime, endTime, dateCourse, control, id_Resource, id_Tp, id_Td, id_Promotion, id_Teacher, id_classroom) VALUES ('10:30:00', '12:30:00', '2023-11-15', true, 2, 2, 2, 2, 2, 2);
+INSERT INTO Courses (startTime, endTime, dateCourse, control, id_Resource, id_Tp, id_Td, id_Promotion, id_Teacher, id_classroom) VALUES ('08:00:00', '10:00:00', '2023-11-15', true, 1, 1, 1, 1, 4, 1);
+INSERT INTO Courses (startTime, endTime, dateCourse, control, id_Resource, id_Tp, id_Td, id_Promotion, id_Teacher, id_classroom) VALUES ('10:30:00', '12:30:00', '2023-11-15', true, 2, 2, 2, 2, 4, 2);
 
 -- Insertion des étudiants
-INSERT INTO Students (nip, ine, apprentice, id_User, id_Td, id_Tp, id_Promotion) VALUES (123456,789455, true, 2, 1, 1, 1);
-INSERT INTO Students (nip, ine, apprentice, id_User, id_Td, id_Tp, id_Promotion) VALUES (789123,123456, false, 1, 2, 2, 2);
+INSERT INTO Students (nip, ine, apprentice, id_User, id_Td, id_Tp, id_Promotion) VALUES (123456,789455, true, 3, 1, 1, 1);
 
 -- Insertion des absences
 INSERT INTO Absences (id_Student, id_Course, reason, justify) VALUES (1, 1, 'Malade', false);
-INSERT INTO Absences (id_Student, id_Course, reason, justify) VALUES (2, 2, 'Raison personnelle', true);
 
 -- Insertion des logs
 INSERT INTO Logs (id_User, modification) VALUES (1, 'Modification 1');
-INSERT INTO Logs (id_User, modification) VALUES (2, 'Modification 2');
+INSERT INTO Logs (id_User, modification) VALUES (1, 'Modification 2');
 
 -- Insertion des rappels
 INSERT INTO Reminder (user_id, reminder_text, reminder_date) VALUES (1, 'Rappel pour la réunion', '2023-12-01 09:00:00');
@@ -69,7 +68,6 @@ INSERT INTO Reminder (user_id, reminder_text, reminder_date) VALUES (2, 'Rappel 
 
 -- Insertion des commentaires
 INSERT INTO Commentary (teacher_id, course_id, comment_text) VALUES (1, 1, 'Très bon travail des étudiants.');
-INSERT INTO Commentary (teacher_id, course_id, comment_text) VALUES (2, 2, 'Nécessite des améliorations, veuillez réviser.');
 
 -- Création du déclencheur
 CREATE OR REPLACE FUNCTION delete_absences_on_student_delete()
@@ -85,16 +83,4 @@ BEFORE DELETE ON ent.Students
 FOR EACH ROW
 EXECUTE FUNCTION delete_absences_on_student_delete();
 
-CREATE OR REPLACE FUNCTION delete_courses_on_teacher_delete()
-RETURNS TRIGGER AS $$
-BEGIN
-    DELETE FROM ent.Courses WHERE id_Teacher = OLD.id;
-    RETURN OLD;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE OR REPLACE TRIGGER delete_courses_trigger
-BEFORE DELETE ON ent.Teachers
-FOR EACH ROW
-EXECUTE FUNCTION delete_courses_on_teacher_delete();
 
